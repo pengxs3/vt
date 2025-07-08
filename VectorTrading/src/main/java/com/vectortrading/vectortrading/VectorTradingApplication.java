5fec14e6ed400b24059cc3e9647ca49022b3f64b
pengxs3 <129406760+pengxs3@users.noreply.github.com>
2025-07-08 13:50:48 +0800

    Add files via upload
    初始化项目

---
+++ VectorTrading/src/main/java/com/vectortrading/vectortrading/VectorTradingApplication.java
@@ -0,0 +1,12 @@
+package com.vectortrading.vectortrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorTradingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VectorTradingApplication.class, args);
+    }
+
+}
---
+++ VectorTrading/src/test/java/com/vectortrading/vectortrading/VectorTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vectortrading.vectortrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
