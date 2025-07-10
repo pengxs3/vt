868fd5cea7e7c28ec71c4ac873c82e97dc2f986d
chenkun194 <chenkun194@jd.com>
2025-07-10 14:46:53 +0800

    尝试 git 提交

--- VectorTrading/src/main/java/com/vectortrading/vectortrading/VectorTradingApplication.java
+++ VectorTrading/src/main/java/com/vectortrading/vectortrading/VectorTradingApplication.java
@@ -2,8 +2,10 @@ package com.vectortrading.vectortrading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(cn.hutool.extra.spring.SpringUtil.class)
 public class VectorTradingApplication {
     public static void main(String[] args) {
         SpringApplication.run(VectorTradingApplication.class, args);
