b230d2f71d97d6f7ad7718f3c63adc3200261e2c
chenkun194 <chenkun194@jd.com>
2025-07-10 15:28:32 +0800

    服务启动+定时任务

--- VectorTrading/src/main/java/com/vectortrading/vectortrading/VectorTradingApplication.java
+++ VectorTrading/src/main/java/com/vectortrading/vectortrading/VectorTradingApplication.java
@@ -2,13 +2,27 @@ package com.vectortrading.vectortrading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
-@SpringBootApplication
+import java.util.Date;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class})
 @Import(cn.hutool.extra.spring.SpringUtil.class)
+@EnableScheduling
 public class VectorTradingApplication {
     public static void main(String[] args) {
         SpringApplication.run(VectorTradingApplication.class, args);
     }
 
+    @Scheduled(fixedRate = 10 * 1000)
+    public void testMethod() {
+        System.out.println("每10秒执行一次: " + new Date());
+    }
 }
